f334f539f88ddd92eba931897fb3edac968bb97e
pete <petersyjuco@gmail.com>
2020-03-27 18:50:00 +0800

    Daily Kata: Quick Sort

--- Daily Kata/main.cpp
+++ Daily Kata/main.cpp
@@ -1,54 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#define MAX_N 31
-
-void merge(int* A, int p, int q, int r) {
-	int i, j, k;
-
-	int n1 = q - p + 1;
-	int n2 = r - q;
-
-	int* L = new int[n1 + 1];
-	int* R = new int[n2 + 1];
-
-	for (i = 1; i <= n1; ++i) L[i] = A[i + p - 1];
-	for (j = 1; j <= n2; ++j) R[j] = A[j + q];
-
-	L[n1 + 1] = MAX_N;
-	R[n2 + 1] = MAX_N;
-
-	i = 1;
-	j = 1;
-	for (k = p; k <= r; ++k) {
-		if (L[i] <= R[j]) {
-			A[k] = L[i];
-			++i;
-		}
-		else {
-			A[k] = R[j];
-			++j;
-		}
-	}
+void swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
-void merge_sort(int* A, int p, int r) {
-	if (p < r) {
-		int q = (p + r) / 2;
-		merge_sort(A, p, q);
-		merge_sort(A, q + 1, r);
-		merge(A, p, q, r);
+int partition(int* A, int start, int end) {
+	int pivot = A[end];
+	int x = start - 1;
+
+	for (int i = start; i <= end; ++i)
+		if (A[i] < pivot)
+			swap(&A[++x], &A[i]);
+
+	swap(&A[++x], &A[end]);
+	return x;
+}
+
+void quick_sort(int* A, int start, int end) {
+	if (start < end) {
+		int p = partition(A, start, end);
+		quick_sort(A, start, p - 1);
+		quick_sort(A, p + 1, end);
 	}
 }
 
 int main() {
-	int num[] = { 5,3,8,1,0,6,7,4,2 };
-	int size = sizeof(num) / 4;
+	int arr[] = { 2,4,5,7,1,2,3,6 };
+	int size = sizeof(arr) / sizeof(arr)[0];
 
-	for (int i = 0; i < size; ++i) printf("%d ", num[i]);
+	for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
 	printf("\n");
-	merge_sort(num, 0, size - 1);
-	for (int i = 0; i < size; ++i) printf("%d ", num[i]);
+	quick_sort(arr, 0, size - 1);
+	for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
 	printf("\n");
 	return 0;
 }
