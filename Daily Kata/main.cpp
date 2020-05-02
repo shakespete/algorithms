2166ddd1401e5de8590edfb9f0514702e20678e3
pete <petersyjuco@gmail.com>
2020-05-02 23:10:08 +0800

    Daily Kata: Insertion Sort

--- Daily Kata/main.cpp
+++ Daily Kata/main.cpp
@@ -1,39 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void swap(int* a, int* b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-int partition(int* A, int start, int end) {
-	int pivot = A[end];
-	int x = start - 1;
-	for (int i = start; i <= end; ++i)
-		if (A[i] < pivot)
-			swap(&A[i], &A[++x]);
-
-	swap(&A[++x], &A[end]);
-	return x;
-}
-
-void quick_sort(int* A, int start, int end) {
-	if (start < end) {
-		int p = partition(A, start, end);
-		quick_sort(A, start, p - 1);
-		quick_sort(A, p + 1, end);
+void insertion_sort(int* A, int N) {
+	for (int j = 1; j < N; ++j) {
+		int key = A[j];
+		int i = j - 1;
+		while (i >= 0 && A[i] > key) {
+			A[i + 1] = A[i];
+			--i;
+		}
+		A[i + 1] = key;
 	}
 }
 
 int main() {
-	int arr[] = { 2,4,5,7,1,0,8,3,6 };
+	int arr[] = { 2,4,8,5,7,9,1,0,3,6 };
 	int size = sizeof(arr) / sizeof(arr)[0];
 
 	for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
 	printf("\n");
-	quick_sort(arr, 0, size - 1);
+	insertion_sort(arr, size);
 	for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
 	printf("\n");
+	return 0;
+
 	return 0;
 }
