0d5b7039c34ed18d972cd69180f76d64e80b5169
pete <petersyjuco@gmail.com>
2020-06-01 21:50:12 +0800

    Daily Kata: Insertion Sort

--- Daily Kata/main.cpp
+++ Daily Kata/main.cpp
@@ -1,53 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#define MAX_N 100
-
-void merge(int* A, int p, int q, int r) {
-	int i, j, k;
-
-	int n1 = q - p + 1;
-	int n2 = r - q;
-
-	int* L = new int[n1 + 1];
-	int* R = new int[n2 + 1];
-
-	for (i = 1; i <= n1; ++i) L[i] = A[i + p - 1];
-	for (j = 1; j <= n2; ++j) R[j] = A[j + q];
-
-	L[n1 + 1] = MAX_N;
-	R[n2 + 1] = MAX_N;
-
-	i = j = 1;
-	for (k = p; k <= r; ++k) {
-		if (L[i] < R[j]) {
-			A[k] = L[i];
-			++i;
+void insertion_sort(int* A, int N) {
+	for (int j = 1; j < N; ++j) {
+		int key = A[j];
+		int i = j - 1;
+		while (i >= 0 && A[i] > key) {
+			A[i + 1] = A[i];
+			--i;
 		}
-		else {
-			A[k] = R[j];
-			++j;
-		}
-	}
-}
-
-void merge_sort(int* A, int p, int r) {
-	if (p < r) {
-		int q = (p + r) / 2;
-		merge_sort(A, p, q);
-		merge_sort(A, q + 1, r);
-		merge(A, p, q, r);
+		A[i + 1] = key;
 	}
 }
 
 int main() {
-	int arr[] = { 2,4,5,7,1,9,8,0,10,11,3,6 };
+	int arr[] = { 10,9,8,0,4,5,7,1,2,3,6 };
 	int size = sizeof(arr) / sizeof(arr)[0];
 
 	for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
 	printf("\n");
-	merge_sort(arr, 0, size - 1);
+	insertion_sort(arr, size);
 	for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
 	printf("\n");
-	return 0;;
+	return 0;
 }
