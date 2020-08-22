6736fdbc846299405930ddb5ca230a054b2596af
shakespete <petersyjuco@gmail.com>
2020-08-22 14:09:47 +1000

    Daily Kata: Bubble Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,16 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void insertion_sort(int* A, int N) {
-    for (int j = 1; j < N; ++j) {
-        int key = A[j];
-        int i = j - 1;
-        while (i >= 0 && A[i] > key) {
-            A[i + 1] = A[i];
-            --i;
-        }
-        A[i + 1] = key;
-    }
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubble_sort(int* A, int N) {
+    for (int i = 1; i < N; ++i)
+        for (int j = 0; j < N - 1; ++j)
+            if (A[j] > A[j+1])
+                swap(&A[j], &A[j+1]);
 }
 
 int main() {
@@ -19,7 +20,7 @@ int main() {
 
     for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
     printf("\n");
-    insertion_sort(arr, size);
+    bubble_sort(arr, size);
     for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
     printf("\n");
     return 0;
