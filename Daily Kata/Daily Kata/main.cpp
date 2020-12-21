ccf1c463208c20febf6ab0c6587e299d458bcfb7
shakespete <petersyjuco@gmail.com>
2020-12-21 15:48:05 +1100

    Daily Kata: Bubble Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,27 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#define MAX_N 100
-void insertion_sort(int* A, int N) {
-    for (int j = 1; j < N; ++j) {
-        int key = A[j];
-        int i = j - 1;
-        while (i >= 0 && A[i] > key) {
-            A[i + 1] = A[i];
-            --i;
-        }
-        A[i + 1] = key;
-    }
+class Node {
+public:
+    int val;
+};
+
+void swap(int a, int b, Node** arr) {
+    Node* temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+void bubble_sort(int N, Node** arr) {
+    for (int i = 1; i < N; ++i)
+        for (int j = 0; j < N - i; ++j)
+            if (arr[j]->val > arr[j+1]->val)
+                swap(j, j + 1, arr);
 }
 
 int main() {
-    int arr[] = { 2,4,5,9,0,7,1,8,3,6 };
-    int size = sizeof(arr) / sizeof(arr)[0];
-
-    for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
+    int num[] = { 4,2,7,9,1,6,3,5,8,0 };
+    int N = sizeof(num) / sizeof(num)[0];
+    
+    Node** arr = new Node*[N];
+    for (int i = 0; i < N; ++i) {
+        Node* node = new Node;
+        node->val = num[i];
+        arr[i] = node;
+    }
+    
+    for (int i = 0; i < N; ++i) printf("%d ", arr[i]->val);
     printf("\n");
-    insertion_sort(arr, size);
-    for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
-    printf("\n");
+    bubble_sort(N, arr);
+    for (int i = 0; i < N; ++i) printf("%d ", arr[i]->val);
+    printf("\nFIN\n");
+    
     return 0;
 }
+
