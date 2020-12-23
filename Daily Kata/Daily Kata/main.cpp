413a04eea3b1239903f1b2efde4d752d9878e526
shakespete <petersyjuco@gmail.com>
2020-12-24 10:53:08 +1100

    Daily Kata: Quick Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -5,37 +5,48 @@ struct Node {
     int val;
 };
 
-void swap (int a, int b, Node** arr) {
-    Node* temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
+void swap(int a, int b, Node** A) {
+    Node* temp = A[a];
+    A[a] = A[b];
+    A[b] = temp;
+};
+
+int partition(int start, int end, Node** A) {
+    Node* pivot = A[end];
+    int x = start - 1;
+    
+    for (int i = start; i <= end; ++i) {
+        if (A[i]->val < pivot->val)
+            swap(i, ++x, A);
+    }
+    swap(end, ++x, A);
+    return x;
 }
 
-void selection_sort (int N, Node** arr) {
-    for (int i = 0; i < N-1; ++i) {
-        int minPtr = i;
-        for (int j = i + 1  ; j < N; ++j) {
-            if (arr[j]->val < arr[minPtr]->val) minPtr = j;
-        }
-        swap(minPtr, i, arr);
+void quick_sort(int start, int end, Node** A) {
+    if (start < end) {
+        int p = partition(start, end, A);
+        quick_sort(start, p-1, A);
+        quick_sort(p+1, end, A);
     }
 }
 
 int main() {
-    int num[] = {3, 1, 6, 8, 2, 4, 9, 0, 5, 7};
-        int N = sizeof(num) / sizeof(num)[0];
+    int num[] = { 4, 1, 8, 9, 0, 3, 6, 1, 7, 5 };
+    int N = sizeof(num) / sizeof(num)[0];
 
-        Node** arr = new Node*[N];
-        for (int i = 0; i < N; ++i) {
-            Node* node = new Node;
-            node->val = num[i];
-            arr[i] = node;
-        }
+    Node** arr = new Node*[N];
+    for (int i = 0; i < N; ++i) {
+        Node* node = new Node;
+        node->val = num[i];
+        arr[i] = node;
+    }
 
-        for (int i = 0; i < N; ++i) printf("%d ", arr[i]->val);
-        selection_sort(N, arr);
-        printf("\n");
-        for (int i = 0; i < N; ++i) printf("%d ", arr[i]->val);
-        printf("\nFIN\n");
+    for (int i = 0; i < N; ++i) printf("%d ", arr[i]->val);
+    quick_sort(0, N - 1, arr);
+    printf("\n");
+    for (int i = 0; i < N; ++i) printf("%d ", arr[i]->val);
+    printf("\nFIN\n");
+
     return 0;
 }
