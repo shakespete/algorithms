c58df2434ece6cc612f4b2138ee11adeb9c269ba
shakespete <petersyjuco@gmail.com>
2021-01-08 10:18:06 +1100

    Daily Kata: Merge Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,50 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-struct Node {
-    int val;
-};
+#define MAX_N 100
 
-void swap(int a, int b, Node** arr) {
-    Node* temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
+void merge(int p, int q, int r, int* A) {
+    int i, j, k;
+    
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    
+    int* L = new int[n1 + 1];
+    int* R = new int[n2 + 1];
+    
+    for (i = 1; i <= n1; ++i) L[i] = A[i + p - 1];
+    for (j = 1; j <= n2; ++j) R[j] = A[j + q];
+    
+    L[n1+1] = MAX_N;
+    R[n2+1] = MAX_N;
+
+    i = j = 1;
+    for (k = p; k <= r; ++k) {
+        if (L[i] <= R[j]) {
+            A[k] = L[i];
+            ++i;
+        } else {
+            A[k] = R[j];
+            ++j;
+        }
+    }
 }
 
-int partition(int start, int end, Node** arr) {
-    Node* pivot = arr[end];
-    int x = start - 1;
-    for (int i = start; i <= end; ++i)
-        if (arr[i]->val < pivot->val)
-            swap(++x, i, arr);
-    swap(++x, end, arr);
-    return x;
-}
-
-void quick_sort(int start, int end, Node** arr) {
-    if (start < end) {
-        int p = partition(start, end, arr);
-        quick_sort(start, p-1, arr);
-        quick_sort(p+1, end, arr);
+void merge_sort(int* A, int p, int r) {
+    if (p < r) {
+        int q = (p + r) / 2;
+        merge_sort(A, p, q);
+        merge_sort(A, q+1 , r);
+        merge(p, q, r, A);
     }
 }
 
 int main() {
-    int num[] = { 4, 1, 8, 9, 0, 3, 6, 2, 7, 5 };
-    int N = sizeof(num) / sizeof(num)[0];
-
-    Node** arr = new Node*[N];
-    for (int i = 0; i < N; ++i) {
-        Node* node = new Node;
-        node->val = num[i];
-        arr[i] = node;
-    }
-
-    for (int i = 0; i < N; ++i) printf("%d ", arr[i]->val);
-    quick_sort(0, N - 1, arr);
-    printf("\n");
-    for (int i = 0; i < N; ++i) printf("%d ", arr[i]->val);
-    printf("\nFIN\n");
+    int arr[] = { 9,0,4,5,7,1,2,3,6 };
+    int size = sizeof(arr) / sizeof(arr)[0];
     
+    for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
+    printf("\n");
+    merge_sort(arr, 0, size - 1);
+    for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
+    printf("\nFIN\n");
     return 0;
 }
