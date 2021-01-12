c03ef934354194ab8efc1af6a5a2013460488f82
shakespete <petersyjuco@gmail.com>
2021-01-12 12:15:30 +1100

    Daily Kata: Quick Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,52 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#define MAX_N 100
-
-void merge(int p, int q, int r, int* A) {
-    int i, j, k;
-    
-    int n1 = q - p + 1;
-    int n2 = r - q;
-    
-    int* L = new int[n1 + 1];
-    int* R = new int[n2 + 1];
-    
-    for (i = 1; i <= n1; ++i) L[i] = A[i + p - 1];
-    for (j = 1; j <= n2; ++j) R[j] = A[j + q];
-    
-    L[n1+1] = MAX_N;
-    R[n2+1] = MAX_N;
-
-    i = j = 1;
-    for (k = p; k <= r; ++k) {
-        if (L[i] <= R[j]) {
-            A[k] = L[i];
-            ++i;
-        } else {
-            A[k] = R[j];
-            ++j;
-        }
-    }
+void swap(int a, int b, int* A) {
+    int temp = A[a];
+    A[a] = A[b];
+    A[b] = temp;
 }
 
-void merge_sort(int* A, int p, int r) {
-    if (p < r) {
-        int q = (p + r) / 2;
-        merge_sort(A, p, q);
-        merge_sort(A, q+1 , r);
-        merge(p, q, r, A);
+int partition(int start, int end, int* A) {
+    int pivot = A[end];
+    int x = start - 1;
+    
+    for (int i = start; i <= end; ++i)
+        if (A[i] < pivot) swap(i, ++x, A);
+    
+    swap(++x, end, A);
+    return x;
+}
+
+void quick_sort(int start, int end, int* A) {
+    if (start < end) {
+        int p = partition(start, end, A);
+        quick_sort(start, p-1,  A);
+        quick_sort(p+1, end, A);
     }
 }
 
 int main() {
-    int arr[] = { 9,0,4,5,7,1,2,3,6 };
-    int size = sizeof(arr) / sizeof(arr)[0];
-    
-    for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
+    int num[] = { 4, 1, 8, 9, 0, 3, 6, 2, 7, 5 };
+    int N = sizeof(num) / sizeof(num)[0];
+    for (int i = 0; i < N; ++i) printf("%d ", num[i]);
+    quick_sort(0, N - 1, num);
     printf("\n");
-    merge_sort(arr, 0, size - 1);
-    for (int i = 0; i < size; ++i) printf("%d ", arr[i]);
+    for (int i = 0; i < N; ++i) printf("%d ", num[i]);
     printf("\nFIN\n");
+    
     return 0;
 }
