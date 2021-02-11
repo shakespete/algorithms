0237c2a0a4b8c1c41d6a4861b6820c8d217eaede
shakespete <petersyjuco@gmail.com>
2021-02-12 08:45:22 +1100

    Daily Kata: Bubble Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,42 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#define MAX_N 100
-    
-void merge(int p, int q, int r, int* A) {
-    int i, j, k;
-    
-    int n1 = q - p + 1;
-    int n2 = r - q;
-    
-    int* L = new int[n1 + 1];
-    int* R = new int[n2 + 1];
-    
-    for (int i = 1; i <= n1; ++i) L[i] = A[i + p - 1];
-    for (int j = 1; j <= n2; ++j) R[j] = A[j + q];
-    
-    L[n1 + 1] = MAX_N;
-    R[n2 + 1] = MAX_N;
-    
-    i = j = 1;
-    for (k = p; k <= r; ++k) {
-        if (L[i] <= R[j]) {
-            A[k] = L[i];
-            ++i;
-        } else {
-            A[k] = R[j];
-            ++j;
-        }
-    }
+void swap(int a, int b, int* A) {
+    int temp = A[a];
+    A[a] = A[b];
+    A[b] = temp;
 }
 
-void merge_sort(int p, int r, int* A) {
-    if (p < r) {
-        int q = (p + r) / 2;
-        merge_sort(p, q, A);
-        merge_sort(q + 1, r, A);
-        merge(p, q, r, A);
-    }
+void bubble_sort(int N , int* A) {
+    for (int i = 1; i < N; ++i)
+        for (int j = 0; j < N - 1; ++j)
+            if (A[j] > A[j+1]) swap(j, j + 1, A);
 }
 
 
@@ -45,7 +19,7 @@ int main() {
     int N = sizeof(num) / sizeof(num)[0];
     
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
-    merge_sort(0, N - 1, num);
+    bubble_sort(N, num);
     printf("\n");
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
     printf("\nFIN\n");
