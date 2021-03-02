a29a985388cc2324ab21337c14efbbb03389c431
shakespete <petersyjuco@gmail.com>
2021-03-03 08:50:47 +1100

    Daily Kata: Selection Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -7,15 +7,13 @@ void swap(int x, int y, int* A) {
     A[y] = temp;
 }
 
-void insertion_sort(int N, int* A) {
-    for (int j = 1; j < N; ++j) {
-        int key = A[j];
-        int i = j - 1;
-        while (i >= 0 && A[i] < key) {
-            A[i + 1] = A[i];
-            --i;
+void selection_sort(int N, int* A) {
+    for (int i = 0; i < N - 1; ++i) {
+        int minPtr = i;
+        for (int j = i + 1; j < N; ++j) {
+            if (A[minPtr] > A[j]) minPtr = j;
+            swap(minPtr, i, A);
         }
-        A[i + 1] = key;
     }
 }
 
@@ -24,7 +22,7 @@ int main() {
     int N = sizeof(num) / sizeof(num)[0];
     
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
-    insertion_sort(N, num);
+    selection_sort(N, num);
     printf("\n");
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
     printf("\nFIN\n");
