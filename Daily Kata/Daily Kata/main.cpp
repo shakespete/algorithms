c9a3a6891c31a9727db93f72da25ca1dcb0361dd
shakespete <petersyjuco@gmail.com>
2021-03-17 09:21:11 +1100

    Daily Kata: Selection Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,17 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void swap(int x, int y, int* A) {
-    int temp = A[x];
-    A[x] = A[y];
-    A[y] = temp;
-}
-
-void bubble_sort(int N, int* A) {
-    for (int i = 1; i < N; ++i)
-        for (int j = 0; j < N - i; ++j)
-            if (A[j] > A[j + 1])
-                swap(j, j + 1, A);
+void selection_sort(int N, int* A) {
+    for (int j = 1; j < N; ++j) {
+        int key = A[j];
+        int i = j - 1;
+        while (i >= 0 && A[i] > key) {
+            A[i + 1] = A[i];
+            --i;
+        }
+        A[i + 1] = key;
+    }
 }
 
 int main() {
@@ -19,7 +18,7 @@ int main() {
     int N = sizeof(num) / sizeof(num)[0];
 
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
-    bubble_sort(N, num);
+    selection_sort(N, num);
     printf("\n");
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
     printf("\nFIN\n");
