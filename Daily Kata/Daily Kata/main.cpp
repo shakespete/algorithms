6063e14e1ae4a4852c366a5971516829eabf3ad7
shakespete <petersyjuco@gmail.com>
2021-04-20 14:00:06 +1000

    Daily Kata: Selection Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,40 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-#define MAX_N 100
-
-void merge(int p, int q, int r, int* A) {
-    int i, j, k;
-    int n1 = q - p + 1;
-    int n2 = r - q;
-    
-    int* L = new int[n1 + 1];
-    int* R = new int[n2 + 1];
-    
-    for (i = 1; i <= n1; ++i) L[i] = A[i + p - 1];
-    for (j = 1; j <= n2; ++j) R[j] = A[j + q];
-    
-    L[n1 + 1] = MAX_N;
-    R[n2 + 1] = MAX_N;
-    
-    i = j = 1;
-    for (k = p; k <= r; ++k) {
-        if (L[i] <= R[j]) {
-            A[k] = L[i];
-            ++i;
-        } else {
-            A[k] = R[j];
-            ++j;
+void selection_sort(int N, int* A) {
+    for(int j = 1; j < N; ++j) {
+        int key = A[j];
+        int i = j - 1;
+        while (i >= 0 && A[i] > key) {
+            A[i + 1] = A[i];
+            --i;
         }
-    }
-}
-
-void merge_sort(int p, int r, int* A) {
-    if (p < r) {
-        int q = (p + r) / 2;
-        merge_sort(p, q, A);
-        merge_sort(q + 1, r, A);
-        merge(p, q, r, A);
+        A[i + 1] = key;
     }
 }
 
@@ -43,7 +18,7 @@ int main() {
     int N = sizeof(num) / sizeof(num)[0];
 
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
-    merge_sort(0, N - 1, num);
+    selection_sort(N, num);
     printf("\n");
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
     printf("\nFIN\n");
