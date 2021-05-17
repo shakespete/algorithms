374651505d9d11b322dfd8dc5b4d04139b5aaab0
shakespete <petersyjuco@gmail.com>
2021-05-17 10:08:27 +1000

    Daily Kata: Insertion Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -7,27 +7,25 @@ void swap(int x, int y, int* A) {
     A[y] = temp;
 }
 
-void selection_sort(int N, int* A) {
-    for (int i = 0; i < N - 1; ++i) {
-        int minPtr = i;
-        for (int j = i + 1; j < N; ++j)
-            if (A[j] < A[minPtr]) minPtr = j;
-        swap(minPtr, i, A);
+void insertion_sort(int N, int* A) {
+    for (int j = 1; j < N; ++j) {
+        int key = A[j];
+        int i = j - 1;
+        while (i >= 0 && A[i] > key) {
+            A[i + 1] = A[i];
+            --i;
+        }
+        A[i + 1] = key;
     }
 }
 
-void bubble_sort(int N, int* A) {
-    for (int i = 1; i < N; ++i)
-        for (int j = 0; j < N - i; ++j)
-            if (A[j] < A[j + 1]) swap(j, j + 1, A);
-}
 
 int main() {
     int num[] = { 4, 1, 8, 9, 0, 3, 6, 2, 7, 5 };
     int N = sizeof(num) / sizeof(num)[0];
 
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
-    bubble_sort(N, num);
+    insertion_sort(N, num);
     printf("\n");
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
     printf("\nFIN\n");
