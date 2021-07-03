c1b7f34b0a96de9b96980b62e0c5db5d2ea1054e
shakespete <petersyjuco@gmail.com>
2021-07-03 15:07:29 +1000

    Daily Kata: Bubble Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,16 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void insertion_sort(int N, int* A) {
-    for (int j = 1; j < N; ++j) {
-        int key = A[j];
-        int i = j - 1;
-        while (i >= 0 && A[i] > key) {
-            A[i + 1] = A[i];
-            --i;
-        }
-        A[i + 1] = key;
-    }
+void swap(int x, int y, int* A) {
+    int temp = A[x];
+    A[x] = A[y];
+    A[y] = temp;
+}
+
+void bubble_sort(int N, int* A) {
+    for (int i = 1; i < N; ++i)
+        for (int j = 0; j < N - i; ++j)
+            if (A[j] < A[j + 1])
+                swap(j, j + 1, A);
 }
 
 int main() {
@@ -18,7 +19,7 @@ int main() {
     int N = sizeof(num) / sizeof(num)[0];
 
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
-    insertion_sort(N, num);
+    bubble_sort(N, num);
     printf("\n");
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
     printf("\nFIN\n");
