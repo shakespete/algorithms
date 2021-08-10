2fb97bd2158d428ebe48f2e4922633b5307bf37f
shakespete <petersyjuco@gmail.com>
2021-08-10 10:49:29 +1000

    Daily Kata: Insertion Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -14,12 +14,24 @@ void bubble_sort(int N, int* A) {
                 swap(j, j + 1, A);
 }
 
+void insertion_sort(int N, int* A) {
+    for (int j = 1; j < N; ++j) {
+        int key = A[j];
+        int i = j - 1;
+        while (i >= 0 && A[i] > key) {
+            A[i + 1] = A[i];
+            --i;
+        }
+        A[i + 1] = key;
+    }
+}
+
 int main() {
     int num[] = { 4, 1, 8, 9, 0, 3, 6, 2, 7, 5 };
     int N = sizeof(num) / sizeof(num)[0];
 
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
-    bubble_sort(N, num);
+    insertion_sort(N, num);
     printf("\n");
     for (int i = 0; i < N; ++i) printf("%d ", num[i]);
     printf("\nFIN\n");
