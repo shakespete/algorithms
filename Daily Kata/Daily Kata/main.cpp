6772f9a79deb22e4f29325fce2a64d724b5028db
shakespete <petersyjuco@gmail.com>
2021-11-01 21:03:50 +1100

    Min Heap

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,52 +4,83 @@
 
 using namespace std;
 
-#define MAX_N 100
+class Heap {
+    enum { DEF_CAP = 100 };
+public:
+    Heap(int cap = DEF_CAP);
+    ~Heap();
+    int size() const;
+    bool empty() const;
+    int root() const;
+    void push(int e);
+    void pop();
+private:
+    int* H;
+    int n;
+    int capacity;
+};
 
-void merge(int p, int q, int r, vector<int> &A) {
-    int i, j, k;
-    
-    int n1 = q - p + 1;
-    int n2 = r - q;
-    
-    int *L = new int[n1 + 1];
-    int *R = new int[n2 + 1];
-    
-    for (i = 1; i <= n1; ++i) L[i] = A[i + p - 1];
-    for (j = 1; j <= n2; ++j) R[j] = A[j + q];
-    
-    L[n1 + 1] = MAX_N;
-    R[n2 + 1] = MAX_N;
-    
-    i = j = 1;
-    for (k = p; k <= r; ++k) {
-        if (L[i] < R[j]) {
-            A[k] = L[i];
-            ++i;
-        } else {
-            A[k] = R[j];
-            ++j;
+Heap::Heap(int cap) : H(new int[cap]), n(0), capacity(cap) { }
+Heap::~Heap() { while (!empty()) pop(); }
+int Heap::size() const { return n; }
+bool Heap::empty() const { return n == 0; }
+int Heap::root() const { return empty() ? INT_MIN : H[1]; }
+void Heap::push(int e) {
+    if (size() != capacity) {
+        n++;
+        H[n] = e;
+        
+        int current = n;
+        while (current > 1 && H[current] < H[current / 2]) {
+            int parent = current / 2;
+            
+            int temp = H[parent];
+            H[parent] = H[current];
+            H[current] = temp;
+            current = parent;
+        }
+    }
+}
+void Heap::pop() {
+    if (!empty()) {
+        H[1] = H[n];
+        --n;
+        
+        int current = 1;
+        while (current * 2 <= n) {
+            int child;
+            int left = current * 2;
+            int right = current * 2 + 1;
+            
+            if (left == n) child = left;
+            else child = H[left] < H[right] ? left : right;
+            
+            if (H[current] < H[child]) break;
+            
+            int temp = H[child];
+            H[child] = H[current];
+            H[current] = temp;
+            current = child;
         }
     }
 }
 
-void mergeSort(int p, int r, vector<int> &A) {
-    if (p < r) {
-        int q = (p + r) / 2;
-        mergeSort(p, q, A);
-        mergeSort(q + 1, r, A);
-        merge(p, q, r, A);
-    }
-}
-
 int main() {
-    vector<int> arr = { 8, 11, 5, 2, 9, 5, 6, 3 };
+    vector<int> arr = { 8, 5, 11, 2, 9, 5, 6, 3 };
     auto N = arr.size();
 
-    for (int i = 0; i < N; ++i) printf("%d ", arr[i]);
-    mergeSort(0, arr.size() - 1, arr);
+    Heap* hp = new Heap();
+    for (int i = 0; i < N; ++i) {
+        printf("%d ", arr[i]);
+        hp->push(arr[i]);
+    }
+    
     printf("\n");
-    for (int i = 0; i < N; ++i) printf("%d ", arr[i]);
+    while (!hp->empty()) {
+        printf("%d ", hp->root());
+        hp->pop();
+    }
+    
     printf("\nFIN\n");
 
     return 0;
