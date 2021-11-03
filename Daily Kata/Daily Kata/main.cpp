86e07ceba197ef20af29382883c6322cda6d55eb
shakespete <petersyjuco@gmail.com>
2021-11-03 12:04:23 +1100

    Heap Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,84 +4,47 @@
 
 using namespace std;
 
-class Heap {
-    enum { DEF_CAP = 100 };
-public:
-    Heap(int cap = DEF_CAP);
-    ~Heap();
-    int size() const;
-    bool empty() const;
-    int root() const;
-    void push(int e);
-    void pop();
-private:
-    int* H;
-    int n;
-    int capacity;
-};
-
-Heap::Heap(int cap) : H(new int[cap]), n(0), capacity(cap) { }
-Heap::~Heap() { while (!empty()) pop(); }
-int Heap::size() const { return n; }
-bool Heap::empty() const { return n == 0; }
-int Heap::root() const { return empty() ? INT_MIN : H[1]; }
-void Heap::push(int e) {
-    if (size() != capacity) {
-        n++;
-        H[n] = e;
-        
-        int current = n;
-        while (current > 1 && H[current] < H[current / 2]) {
-            int parent = current / 2;
-            
-            int temp = H[parent];
-            H[parent] = H[current];
-            H[current] = temp;
-            current = parent;
-        }
+void maxHeapify(int i, vector<int> &array, int heapSize) {
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    
+    int largest;
+    if (l < heapSize && array[l] > array[i]) largest = l;
+    else largest = i;
+    
+    if (r < heapSize && array[r] > array[largest]) largest = r;
+    
+    if (largest != i) {
+        swap(array[i], array[largest]);
+        maxHeapify(largest, array, heapSize);
     }
 }
-void Heap::pop() {
-    if (!empty()) {
-        H[1] = H[n];
-        --n;
-        
-        int current = 1;
-        while (current * 2 <= n) {
-            int child;
-            int left = current * 2;
-            int right = current * 2 + 1;
-            
-            if (left == n) child = left;
-            else child = H[left] < H[right] ? left : right;
-            
-            if (H[current] < H[child]) break;
-            
-            int temp = H[child];
-            H[child] = H[current];
-            H[current] = temp;
-            current = child;
-        }
+void buildMaxHeap(vector<int> &array) {
+    int heapSize = (int)array.size();
+    int parentIdx = heapSize / 2 - 1;
+    for (int i = parentIdx; i >= 0; --i) maxHeapify(i, array, heapSize);
+}
+
+void heapSort(vector<int> &array) {
+    buildMaxHeap(array);
+    int heapSize = (int)array.size();
+    for (int i = heapSize - 1; i > 0; --i) {
+        swap(array[0], array[i]);
+        heapSize--;
+        maxHeapify(0, array, heapSize);
     }
 }
 
 int main() {
-    vector<int> arr = { 8, 5, 11, 2, 9, 5, 6, 3 };
-    auto N = arr.size();
-
-    Heap* hp = new Heap();
-    for (int i = 0; i < N; ++i) {
-        printf("%d ", arr[i]);
-        hp->push(arr[i]);
-    }
-    
+    vector<int> arr = { 4, 1, 9, 10, 14, 8, 7, 3, 2, 16 };
+    for (int i : arr) printf("%d ", i);
     printf("\n");
-    while (!hp->empty()) {
-        printf("%d ", hp->root());
-        hp->pop();
-    }
     
-    printf("\nFIN\n");
+    heapSort(arr);
+    for (int i : arr) printf("%d ", i);
+    printf("\n");
+    
+    printf("FIN\n");
 
     return 0;
 }
