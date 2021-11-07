4aaea0197d35a7a02ae30dcaac853723fe698168
shakespete <petersyjuco@gmail.com>
2021-11-07 20:02:16 +1100

    Heap Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,28 +4,46 @@
 
 using namespace std;
 
-void selectionSort(vector<int> &arr) {
-    int N = (int)arr.size();
+void maxHeapify(int i, vector<int> &arr, int heapSize) {
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
     
-    for (int i = 0; i < N - 1; ++i) {
-        int minPtr = i;
-        for (int j = i + 1; j < N; ++j)
-            if (arr[j] < arr[minPtr]) minPtr = j;
-        
-        swap(arr[minPtr], arr[i]);
+    int largest;
+    if (l < heapSize && arr[l] > arr[i]) largest = l;
+    else largest = i;
+    
+    if (r < heapSize && arr[r] > arr[largest]) largest = r;
+    
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        maxHeapify(largest, arr, heapSize);
+    }
+}
+
+void buildMaxHeap(vector<int> &arr) {
+    int heapSize = (int)arr.size();
+    int parentIdx = heapSize / 2 - 1;
+    for (int i = parentIdx; i >= 0; --i) maxHeapify(i, arr, heapSize);
+}
+
+void heapSort(vector<int> &arr) {
+    buildMaxHeap(arr);
+    int heapSize = (int)arr.size();
+    for (int i = heapSize - 1; i > 0; --i) {
+        swap(arr[0], arr[i]);
+        heapSize--;
+        maxHeapify(0, arr, heapSize);
     }
 }
 
 int main() {
-    vector<int> arr = { 4, 1, 9, 10, 14, 8, 7, 3, 2, 16 };
+    vector<int> arr = { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 };
     for (int i : arr) printf("%d ", i);
     printf("\n");
-    
-    selectionSort(arr);
+    heapSort(arr);
     for (int i : arr) printf("%d ", i);
     printf("\n");
-    
     printf("FIN\n");
-
+    
     return 0;
 }
