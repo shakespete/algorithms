f824145be8081eaceeb3a91dce0baac07de4d876
shakespete <petersyjuco@gmail.com>
2021-11-27 22:01:00 +1100

    Quick Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,29 +4,22 @@
 
 using namespace std;
 
-void insertionSort(vector<int>& arr) {
-    int N = (int)arr.size();
+int partition(int start, int end, vector<int>& A) {
+    int pivot = A[end];
+    int x = start - 1;
+    for (int i = start; i <= end; ++i)
+        if (A[i] < pivot)
+            swap(A[++x], A[i]);
     
-    for (int j = 1; j < N; ++j) {
-        int key = arr[j];
-        int i = j - 1;
-        while (i >= 0 && arr[i] < key) {
-            arr[i + 1] = arr[i];
-            --i;
-        }
-        arr[i + 1] = key;
-    }
+    swap(A[++x], A[end]);
+    return x;
 }
 
-void selectionSort(vector<int>& arr) {
-    int N = (int)arr.size();
-    
-    for (int i = 0; i < N - 1; ++i) {
-        int minPtr = i;
-        for (int j = i + 1; j < N; ++j) {
-            if (arr[j] < arr[minPtr]) minPtr = j;
-        }
-        swap(arr[minPtr], arr[i]);
+void quickSort(int start, int end, vector<int>& A) {
+    if (start < end) {
+        int p = partition(start, end, A);
+        quickSort(start, p - 1, A);
+        quickSort(p + 1, end, A);
     }
 }
 
@@ -34,11 +27,8 @@ int main() {
     vector<int> arr = { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 };
     for (int i : arr) printf("%d ", i);
     printf("\n");
-    insertionSort(arr);
-    for (int i : arr) printf("%d ", i);
     
-    printf("\n");
-    selectionSort(arr);
+    quickSort(0, (int)arr.size() - 1, arr);
     for (int i : arr) printf("%d ", i);
     printf("\nFIN\n");
     
