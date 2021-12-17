5c60a732cae8a5278d5869fe112c36736f8d400a
shakespete <petersyjuco@gmail.com>
2021-12-17 20:51:15 +1100

    Selection/Insertion Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,39 +4,27 @@
 
 using namespace std;
 
-void merge(int p, int q, int r, vector<int>& arr) {
-    int i, j;
-    
-    int n1 = q - p + 1;
-    int n2 = r - q;
-    
-    int* L = new int[n1 + 1];
-    int* R = new int[n2 + 1];
-    
-    for (i = 1; i <= n1; ++i) L[i] = arr[i + p - 1];
-    for (j = 1; j <= n2; ++j) R[j] = arr[j + q];
-    
-    L[n1 + 1] = INT_MAX;
-    R[n2 + 1] = INT_MAX;
-    
-    i = j = 1;
-    for (int k = p; k <= r; ++k) {
-        if (L[i] < R[j]) {
-            arr[k] = L[i];
-            ++i;
-        } else {
-            arr[k] = R[j];
-            ++j;
-        };
+void insertionSort(vector<int>& arr) {
+    int N = (int)arr.size();
+    for (int j = 1; j < N; ++j) {
+        int key = arr[j];
+        int i = j - 1;
+        while (i >= 0 && arr[i] > key) {
+            arr[i + 1] = arr[i];
+            --i;
+        }
+        arr[i + 1] = key;
     }
 }
 
-void mergeSort(int p, int r, vector<int>& arr) {
-    if (p < r) {
-        int q = (p + r) / 2;
-        mergeSort(p, q, arr);
-        mergeSort(q + 1, r, arr);
-        merge(p, q, r, arr);
+void selectionSort(vector<int>& arr) {
+    int N = (int)arr.size();
+    for (int i = 0; i < N - 1; ++i) {
+        int minPtr = i;
+        for (int j = i + 1; j < N; ++j) {
+            if (arr[j] > arr[minPtr]) minPtr = j;
+        }
+        swap(arr[i], arr[minPtr]);
     }
 }
 
@@ -45,7 +33,10 @@ int main() {
     for (int i : arr) printf("%d ", i);
     printf("\n");
     
-    mergeSort(0, (int)arr.size() - 1, arr);
+    insertionSort(arr);
+    for (int i : arr) printf("%d ", i);
+    printf("\n");
+    selectionSort(arr);
     for (int i : arr) printf("%d ", i);
     printf("\nFIN\n");
     
