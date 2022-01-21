8446fac88f291c419f47a40287a66934b2e16d7e
shakespete <petersyjuco@gmail.com>
2022-01-21 18:07:26 +1100

    Bubble Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,44 +4,20 @@
 
 using namespace std;
 
-void maxHeapify(int i, vector<int>& vec, int heapSize) {
-    int l = 2 * i + 1;
-    int r = 2 * i + 2;
+void bubbleSort(vector<int>& vec) {
+    int N = (int)vec.size();
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N - i; ++j)
+            if (vec[j] > vec[j + 1])
+                swap(vec[j], vec[j + 1]);
     
-    int largest = i;
-    if (l < heapSize && vec[l] > vec[largest]) largest = l;
-    if (r < heapSize && vec[r] > vec[largest]) largest = r;
-    
-    if (largest != i) {
-        swap(vec[largest], vec[i]);
-        maxHeapify(largest, vec, heapSize);
-    }
 }
-
-void buildMaxHeap(vector<int>& vec) {
-    int heapSize = (int)vec.size();
-    int parent = heapSize / 2 - 1;
-    for (int i = parent; i >= 0; --i)
-        maxHeapify(i, vec, heapSize);
-}
-
-void heapSort(vector<int>& vec) {
-    buildMaxHeap(vec);
-    int heapSize = (int)vec.size();
-    
-    for (int i = heapSize - 1; i > 0; --i) {
-        swap(vec[0], vec[i]);
-        heapSize--;
-        maxHeapify(0, vec, heapSize);
-    }
-}
-
 int main() {
     vector<int> arr = { 21, 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 };
     for (int i : arr) printf("%d ", i);
     printf("\n");
     
-    heapSort(arr);
+    bubbleSort(arr);
     for (int i : arr) printf("%d ", i);
     
     printf("\nFIN\n");
