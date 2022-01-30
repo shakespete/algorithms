ed60055e0045ab01cc247c7cd6ab61fe2010eb92
shakespete <petersyjuco@gmail.com>
2022-01-30 22:43:17 +1100

    Quick Select

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,49 +1,49 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-void insertionSort(vector<int>& arr) {
-    int N = (int)arr.size();
+int partition(int start, int end, vector<int>& vec) {
+    int pivot = vec[end];
+    int x = start - 1;
+    for (int i = start; i <= end; ++i)
+        if (vec[i] < pivot)
+            swap(vec[++x], vec[i]);
     
-    for (int j = 1; j < N; ++j) {
-        int key = arr[j];
-        int i = j - 1;
-        
-        while (i >= 0 && arr[i] < key) {
-            arr[i + 1] = arr[i];
-            --i;
-        }
-        arr[i + 1] = key;
-    }
+    swap(vec[++x], vec[end]);
+    return x;
 }
 
-void selectionSort(vector<int>& arr) {
-    int N = (int)arr.size();
+void quickSelect(int start, int end, vector<int>& arr, int i) {
+    int p = partition(start, end, arr);
     
-    for (int i = 0; i < N - 1; ++i) {
-        int minPtr = i;
-        for (int j = i + 1; j < N; ++j)
-            if (arr[j] < arr[minPtr])
-                minPtr = j;
-        
-        swap(arr[i], arr[minPtr]);
-    }
+    if (i == p) return;
+    
+    if (i > p) quickSelect(p + 1, end, arr, i);
+    else quickSelect(start, p - 1, arr, i);
+}
+
+int kthLargest(vector<int>& arr, int k) {
+    int N = (int)arr.size();
+    // 1 2 3 4 5 n
+    // 0 1 2 3 4 5
+    int kthSmallest = N - k; // => refers to index of kth smallest
+    
+    quickSelect(0, N - 1, arr, kthSmallest);
+    return arr[kthSmallest];
 }
 
 int main() {
-    vector<int> arr = { 4, 21, 1, 3, 2, 16, 9, 10, 14, 8, 7 };
-    for (int i : arr) printf("%d ", i);
-    printf("\n");
+    vector<int> arr{ 3, 7, 11, 2, 17, 7, 1, 5, 8 };
     
-    insertionSort(arr);
-    for (int i : arr) printf("%d ", i);
-    printf("\n");
-    selectionSort(arr);
-    for (int i : arr) printf("%d ", i);
+    int k = 3;
+    int ans = kthLargest(arr, k);
     
+    for (auto& i : arr) cout << i << " ";
+    cout << "\n";
+    
+    cout << k << "rd Largest: " << ans << "\n";
     printf("\nFIN\n");
-    
     return 0;
 }
