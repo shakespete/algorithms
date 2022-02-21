6002dd0d91ac180f6e2545c34d95fe3326196fc0
shakespete <petersyjuco@gmail.com>
2022-02-21 21:20:24 +1100

    Insertion/Selection Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,32 +4,30 @@
 
 using namespace std;
 
-int partition(int start, int end, vector<int>& arr) {
-    int pivot = arr[end];
-    int x = start - 1;
-    for (int i = start; i <= end; ++i)
-        if (arr[i] < pivot)
-            swap(arr[++x], arr[i]);
-    
-    swap(arr[++x], arr[end]);
-    return x;
-}
-
-void quickSelect(int start, int end, vector<int>& arr, int i) {
-    int p = partition(start, end, arr);
-    
-    if (i == p) return;
-    
-    if (i > p) quickSelect(p + 1, end, arr, i);
-    else quickSelect(start, p - 1, arr, i);
-}
-
-int kthLargest(vector<int>& arr, int k) {
+void selectionSort(vector<int>& arr) {
     int N = (int)arr.size();
-    int kthSmallest = N - k;
     
-    quickSelect(0, N - 1, arr, kthSmallest);
-    return arr[kthSmallest];
+    for (int i = 0; i < N - 1; ++i) {
+        int minPtr = i;
+        for (int j = i + 1; j < N; ++j)
+            if (arr[j] < arr[minPtr])
+                minPtr = j;
+        swap(arr[i], arr[minPtr]);
+    }
+}
+
+void insertionSort(vector<int>& arr) {
+    int N = (int)arr.size();
+    
+    for (int j = 1; j < N; ++j) {
+        int key = arr[j];
+        int i = j - 1;
+        while (i >= 0 && arr[i] < key) {
+            arr[i + 1] = arr[i];
+            --i;
+        }
+        arr[i + 1] = key;
+    }
 }
 
 int main() {
@@ -38,10 +36,12 @@ int main() {
     for (auto& i : arr) cout << i << " ";
     
     cout << "\n";
-    int k = 7;
-    int ans = kthLargest(arr, k);
+    insertionSort(arr);
+    for (auto& i : arr) cout << i << " ";
     
-    cout << k << ": " << ans;
+    cout << "\n";
+    selectionSort(arr);
+    for (auto& i : arr) cout << i << " ";
     
     printf("\nFIN\n");
     return 0;
