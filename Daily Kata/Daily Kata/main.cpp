948482ee6d774d23b503d25f0a5708a5e5f30ade
shakespete <petersyjuco@gmail.com>
2022-03-04 22:03:29 +1100

    KMP Algo

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,37 +1,62 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-int partition(int start, int end, vector<int>& arr) {
-    int pivot = arr[end];
-    int x = start - 1;
-    for (int i = start; i <= end; ++i)
-        if (arr[i] < pivot)
-            swap(arr[++x], arr[i]);
+vector<int> buildPattern(string substr) {
+    vector<int> pattern(substr.size(), -1);
+    int i = 1;
+    int j = 0;
     
-    swap(arr[++x], arr[end]);
-    return x;
+    while (i < substr.size()) {
+        if (substr[i] == substr[j]) {
+            pattern[i] = j;
+            ++i;
+            ++j;
+        } else if (j > 0) {
+            j = pattern[j - 1] + 1;
+        } else {
+            ++i;
+        }
+    }
+    
+    return pattern;
 }
 
-void quickSort(int start, int end, vector<int>& arr) {
-    if (start < end) {
-        int p = partition(start, end, arr);
-        quickSort(start, p - 1, arr);
-        quickSort(p + 1, end, arr);
+bool KMPAlgo(string str, string substr) {
+    vector<int> pattern = buildPattern(substr);
+    
+    int N = (int)substr.size();
+    int i = 0;
+    int j = 0;
+    
+    while (i + N - j <= str.size()) {
+        if (str[i] == substr[j]) {
+            if (j == N - 1) return true;
+            ++i;
+            ++j;
+        } else if (j > 0) {
+            j = pattern[j - 1] + 1;
+        } else {
+            ++i;
+        }
     }
+    
+    return false;
 }
 
 int main() {
     vector<int> arr{ 39, 3, 7, 11, 2, 17, 7, 1, 5, 21, 8 };
-    // 1, 2, 3, 5, 7, 7, 8, 11, 17, 21, 39
-    for (auto& i : arr) cout << i << " ";
+//    1, 2, 3, 5, 7, 7, 8, 11, 17, 21, 39
+//    for (auto& i : arr) cout << i << " ";
     
-    cout << "\n";
-    quickSort(0, (int)arr.size() - 1, arr);
-    for (auto& i : arr) cout << i << " ";
+//    cout << "\n";
+//    for (auto& i : arr) cout << i << " ";
     
-    printf("\nFIN\n");
+    cout << KMPAlgo("abxabcabcaby", "abcaby") << "\n";
+    cout << KMPAlgo("decadaafcdf", "daf") << "\n";
+    printf("FIN\n");
     return 0;
 }
