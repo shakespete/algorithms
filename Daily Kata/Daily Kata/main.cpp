d2a767fb432d33fe40bbb4a1b369c4f4bc673455
shakespete <petersyjuco@gmail.com>
2022-03-08 22:04:01 +1100

    Merge Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -1,62 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <string>
 #include <vector>
 
 using namespace std;
 
-vector<int> buildPattern(string substr) {
-    vector<int> pattern(substr.size(), -1);
-    int i = 1;
-    int j = 0;
+void merge(int p, int q, int r, vector<int>& arr) {
+    int i, j;
     
-    while (i < substr.size()) {
-        if (substr[i] == substr[j]) {
-            pattern[i] = j;
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    
+    int* L = new int[n1 + 1];
+    int* R = new int[n2 + 1];
+    
+    for (i = 1; i <= n1; ++i) L[i] = arr[i + p - 1];
+    for (j = 1; j <= n2; ++j) R[j] = arr[j + q];
+    
+    L[n1 + 1] = INT_MAX;
+    R[n2 + 1] = INT_MAX;
+    
+    i = j = 1;
+    for (int k = p; k <= r; ++k) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
             ++i;
-            ++j;
-        } else if (j > 0) {
-            j = pattern[j - 1] + 1;
         } else {
-            ++i;
+            arr[k] = R[j];
+            ++j;
         }
     }
-    
-    return pattern;
 }
 
-bool KMPAlgo(string str, string substr) {
-    vector<int> pattern = buildPattern(substr);
-    
-    int N = (int)substr.size();
-    int i = 0;
-    int j = 0;
-    
-    while (i + N - j <= str.size()) {
-        if (str[i] == substr[j]) {
-            if (j == N - 1) return true;
-            ++i;
-            ++j;
-        } else if (j > 0) {
-            j = pattern[j - 1] + 1;
-        } else {
-            ++i;
-        }
+void merge_sort(int p, int r, vector<int>& arr) {
+    if (p < r) {
+        int q = (p + r) / 2;
+        merge_sort(p, q, arr);
+        merge_sort(q + 1, r, arr);
+        merge(p, q, r, arr);
     }
-    
-    return false;
 }
 
 int main() {
     vector<int> arr{ 39, 3, 7, 11, 2, 17, 7, 1, 5, 21, 8 };
 //    1, 2, 3, 5, 7, 7, 8, 11, 17, 21, 39
-//    for (auto& i : arr) cout << i << " ";
+    for (auto& i : arr) cout << i << " ";
     
-//    cout << "\n";
-//    for (auto& i : arr) cout << i << " ";
+    cout << "\n";
+    merge_sort(0, (int)arr.size() - 1, arr);
+    for (auto& i : arr) cout << i << " ";
+    cout << "\n";
     
-    cout << KMPAlgo("abxabcabcaby", "abcaby") << "\n";
-    cout << KMPAlgo("decadaafcdf", "daf") << "\n";
     printf("FIN\n");
     return 0;
 }
