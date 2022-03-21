e316022fb3ee7507eaddf1d6c4412fc81a9c56f3
shakespete <petersyjuco@gmail.com>
2022-03-21 21:50:38 +1100

    Quick Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,12 +4,22 @@
 
 using namespace std;
 
-void bubble_sort(vector<int>& arr) {
-    int N = (int)arr.size();
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N - i; ++j)
-            if (arr[j] > arr[j + 1])
-                swap(arr[j], arr[j + 1]);
+int partition(int start, int end, vector<int>& arr) {
+    int pivot = arr[end];
+    int x = start - 1;
+    for (int i = start; i <= end; ++i)
+        if (arr[i] < pivot)
+            swap(arr[++x], arr[i]);
+    swap(arr[++x], arr[end]);
+    return x;
+}
+
+void quick_sort(int start, int end, vector<int>& arr) {
+    if (start < end) {
+        int p = partition(start, end, arr);
+        quick_sort(start, p - 1, arr);
+        quick_sort(p + 1, end, arr);
+    }
 }
 
 int main() {
@@ -18,7 +28,7 @@ int main() {
     for (auto& i : arr) cout << i << " ";
     
     cout << "\n";
-    bubble_sort(arr);
+    quick_sort(0, (int)arr.size() - 1, arr);
     for (auto& i : arr) cout << i << " ";
     cout << "\n";
     
