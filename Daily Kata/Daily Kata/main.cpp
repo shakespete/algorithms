855b8c97e9c0416052dc53a24b9bd30935d93b7e
shakespete <petersyjuco@gmail.com>
2022-04-12 21:15:12 +1000

    Insertion Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,21 +4,25 @@
 
 using namespace std;
 
-void bubble_sort(vector<int>& arr) {
+void insertion_sort(vector<int>& arr) {
     int N = (int)arr.size();
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N - i; ++j)
-            if (arr[j] > arr[j + 1])
-                swap(arr[j], arr[j + 1]);
+    for (int j = 1; j < N; ++j) {
+        int key = arr[j];
+        int i = j - 1;
+        while (i >= 0 && arr[i] > key) {
+            arr[i + 1] = arr[i];
+            --i;
+        }
+        arr[i + 1] = key;
+    }
 }
-
 int main() {
     vector<int> arr{ 39, 3, 7, 11, 2, 17, 7, 1, 5, 21, 8 };
 //    1, 2, 3, 5, 7, 7, 8, 11, 17, 21, 39
     for (auto& i : arr) cout << i << " ";
     
     cout << "\n";
-    bubble_sort(arr);
+    insertion_sort(arr);
     for (auto& i : arr) cout << i << " ";
     cout << "\n";
     
