a6282f8a6efb7830f1eb5c8384ff14d0e743ad6d
shakespete <petersyjuco@gmail.com>
2022-04-16 22:38:44 +1000

    Quick Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,25 +4,32 @@
 
 using namespace std;
 
-void insertion_sort(vector<int>& arr) {
-    int N = (int)arr.size();
-    for (int j = 1; j < N; ++j) {
-        int key = arr[j];
-        int i = j - 1;
-        while (i >= 0 && arr[i] > key) {
-            arr[i + 1] = arr[i];
-            --i;
-        }
-        arr[i + 1] = key;
+int partition(int start, int end, vector<int>& arr) {
+    int pivot = arr[end];
+    int x = start - 1;
+    for (int i = start; i <= end; ++i)
+        if (arr[i] < pivot)
+            swap(arr[++x], arr[i]);
+    
+    swap(arr[++x], arr[end]);
+    return x;
+}
+
+void quick_sort(int start, int end, vector<int>& arr) {
+    if (start < end) {
+        int p = partition(start, end, arr);
+        quick_sort(start, p - 1, arr);
+        quick_sort(p + 1, end, arr);
     }
 }
+
 int main() {
     vector<int> arr{ 39, 3, 7, 11, 2, 17, 7, 1, 5, 21, 8 };
 //    1, 2, 3, 5, 7, 7, 8, 11, 17, 21, 39
     for (auto& i : arr) cout << i << " ";
     
     cout << "\n";
-    insertion_sort(arr);
+    quick_sort(0, (int)arr.size() - 1, arr);
     for (auto& i : arr) cout << i << " ";
     cout << "\n";
     
