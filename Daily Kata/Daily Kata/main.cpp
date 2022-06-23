6a441a6ec2a94e5a552c8159e0b88ac76bbbd756
shakespete <petersyjuco@gmail.com>
2022-06-23 23:08:38 +1000

    Quick Sort

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -14,37 +14,22 @@ int partition(int start, int end, vector<int>& arr) {
     return x;
 }
 
-void quickSelect(int start, int end, vector<int>& arr, int i) {
-    int p = partition(start, end, arr);
-    cout << "i: " << i << "\n";
-    cout << "p: " << p << "\n";
-    
-    for (auto& e : arr) cout << e << " ";
-    
-    cout << "\n";
-    
-    if (i == p) return;
-    
-    if (i > p) quickSelect(p + 1, end, arr, i);
-    else quickSelect(start, p - 1, arr, i);
-}
-
-int kthLargest(vector<int>& arr, int k) {
-    int N = (int)arr.size();
-    int kthSmallest = N - k;
-    
-    quickSelect(0, N - 1, arr, kthSmallest);
-    return arr[kthSmallest];
+void quickSort(int start, int end, vector<int>& arr) {
+    if (start < end) {
+        int p = partition(start, end, arr);
+        quickSort(start, p - 1, arr);
+        quickSort(p + 1, end, arr);
+    }
 }
 
 int main() {
     vector<int> arr{ 39, 3, 7, 11, 2, 17, 7, 1, 5, 21, 8 };
 //    1, 2, 3, 5, 7, 7, 8, 11, 17, 21, 39
     for (auto& i : arr) cout << i << " ";
-    
     cout << "\n";
-    cout << kthLargest(arr, 3);
-//    for (auto& i : arr) cout << i << " ";
+    
+    quickSort(0, (int)arr.size() - 1, arr);
+    for (auto& i : arr) cout << i << " ";
     cout << "\n";
     
     printf("FIN\n");
