38295d5feb93c625a74a495c0ccc96214c35ac12
Peter Syjuco <petersyjuco@gmail.com>
2022-07-29 22:08:21 +1000

    DFS

--- Daily Kata/Daily Kata/main.cpp
+++ Daily Kata/Daily Kata/main.cpp
@@ -4,52 +4,65 @@
 
 using namespace std;
 
-void merge(int p, int q, int r, vector<int>& arr) {
-    int i, j;
-    
-    int n1 = q - p + 1;
-    int n2 = r - q;
-    
-    int* L = new int[n1 + 1];
-    int* R = new int[n2 + 1];
-    
-    for (i = 1; i <= n1; ++i) L[i] = arr[i + p - 1];
-    for (j = 1; j <= n2; ++j) R[j] = arr[j + q];
-    
-    L[n1 + 1] = INT_MAX;
-    R[n2 + 1] = INT_MAX;
-    
-    i = j = 1;
-    for (int k = p; k <= r; ++k) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            ++i;
-        } else {
-            arr[k] = R[j];
-            ++j;
-        }
-    }
-}
+class Graph {
+public:
+    Graph(int e);
+    void addEdge(int v, int w);
+    void DFS();
+    void visit(int u);
+private:
+    int V;
+    int** adjList;
+    int* visited;
+};
 
-void mergeSort(int p, int r, vector<int>& arr) {
-    if (p < r) {
-        int q = (p + r) / 2;
-        mergeSort(p, q, arr);
-        mergeSort(q + 1, r, arr);
-        merge(p, q, r, arr);
-    }
+
+Graph::Graph(int e) {
+    V = e;
+    adjList = new int*[e];
+    visited = new int[e]{0};
+    for (int i = 0; i < e; ++i)
+        adjList[i] = new int[e]{0};
+}
+void Graph::addEdge(int v, int w) {
+    adjList[v][w] = 1;
+    adjList[w][v] = 1;
+}
+void Graph::DFS() {
+    for (int i = 1; i < V; ++i)
+        if (visited[i] != 1)
+            visit(i);
+            
+}
+void Graph::visit(int u) {
+    visited[u] = 1;
+    cout << u << " ";
+    for (int i = 1; i < V; ++i)
+        if (adjList[u][i] == 1)
+            if (visited[i] != 1)
+                visit(i);
 }
 
 int main() {
-    vector<int> arr{ 39, 3, 7, 11, 2, 17, 7, 1, 5, 21, 8 };
-//    1, 2, 3, 5, 7, 7, 8, 11, 17, 21, 39
-    for (auto& i : arr) cout << i << " ";
-    cout << "\n";
+//    vector<int> arr{ 39, 3, 7, 11, 2, 17, 7, 1, 5, 21, 8 };
+////    1, 2, 3, 5, 7, 7, 8, 11, 17, 21, 39
+//    for (auto& i : arr) cout << i << " ";
+//    cout << "\n";
+//
+//    mergeSort(0, (int)arr.size() - 1, arr);
+//    for (auto& i : arr) cout << i << " ";
+//    cout << "\n";
+    Graph* g = new Graph(10);
+    g->addEdge(1, 2);
+    g->addEdge(2, 3);
+    g->addEdge(3, 4);
+    g->addEdge(1, 5);
+    g->addEdge(5, 6);
+    g->addEdge(6, 7);
+    g->addEdge(6, 8);
+    g->addEdge(1, 9);
+    g->DFS();
     
-    mergeSort(0, (int)arr.size() - 1, arr);
-    for (auto& i : arr) cout << i << " ";
-    cout << "\n";
-    
-    printf("FIN\n");
+    cout << "\nFIN\n";
     return 0;
 }
